53686bcdfa9724c9b8919c07d1b7c622720073bd
Svetlana <svetik0814@mail.ru>
2022-09-21 23:24:58 +0500

    new files

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,3 @@
-import java.sql.DriverManager.println
-
 fun main(){
     val likes = 15
     val wordForUsers = if (likes == 0) "никому" else if (likes % 10 == 1) "человеку" else if (likes % 10 == 2 || likes % 10 == 3 ) "людям" else "человекам"
